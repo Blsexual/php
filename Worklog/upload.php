ce17a02d836a84446839c4dc9660e4c94700bf71
Blsexual <kevin.johansson@elev.ga.ntig.se>
2022-09-01 11:16:09 +0200

    Added new files. MAde minor changes to upload

--- Worklog/upload.php
+++ Worklog/upload.php
@@ -1,66 +1,76 @@
-<?php
-    session_start();
-    require_once("db.php");
-    $Checked = FALSE;
-    $Today = date("Y-m-d");
+<div id='fuckyoupost'>
+    <?php
+        session_start();
+        require_once("db.php");
+        echo '<link rel="stylesheet" href="style.css">';
+        $Checked = FALSE;
+        $Today = date("Y-m-d");
+        $CurrentTime = date("h:i");
 
-    if (!empty($_POST)){
-        $Date = $_POST['Date'];
-        $TimeS = $_POST['TimeS'];
-        $TimeE = $_POST['TimeE'];
-        $Kommentar = $_POST['Kommentar'];
+
+        
+        if (!empty($_POST) || ($_POST['Kommentar'] != NULL)){
+            $Date = $_POST['Date'];
+            $TimeS = $_POST['TimeS'];
+            $TimeE = $_POST['TimeE'];
+            $Kommentar = $_POST['Kommentar'];
+
+            $sql = "SELECT * FROM worklog";
+            if($result = mysqli_query($conn, $sql)){
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_array($result)){
+                        if (($Date == $row['Date']) && ($TimeS == $row['TimeS']) && ($TimeE == $row['TimeE']) && ($Kommentar == $row['Comment'])) {
+                            $Checked = TRUE;
+                        }
+                    }    
+                    if ($Checked == FALSE) {
+                        $sql2 = "INSERT INTO `worklog` (`ID`, `TimeS`, `TimeE`, `Date`, `Comment`) VALUES (NULL, '".$TimeS."', '".$TimeE."', '".$Date."', '".$Kommentar."')";
+                        if ($conn->query($sql2) === TRUE) {
+                            echo "New record created successfully";
+                        } 
+                        else {
+                            echo "Error: " . $sql2 . "<br>" . $conn->error;
+                        } 
+                    }
+                } 
+                else{
+                    echo "No match for Query";
+                }
+            }
+        }
+
 
         $sql = "SELECT * FROM worklog";
         if($result = mysqli_query($conn, $sql)){
             if(mysqli_num_rows($result) > 0){
                 while($row = mysqli_fetch_array($result)){
-                    if (($Date == $row['Date']) && ($TimeS == $row['TimeS']) && ($TimeE == $row['TimeE']) && ($Kommentar == $row['Comment'])) {
-                        $Checked = TRUE;
-                    }
-                }    
-                if ($Checked == FALSE) {
-                    $sql2 = "INSERT INTO `worklog` (`ID`, `TimeS`, `TimeE`, `Date`, `Comment`) VALUES (NULL, '".$TimeS."', '".$TimeE."', '".$Date."', '".$Kommentar."')";
-                    if ($conn->query($sql2) === TRUE) {
-                        echo "New record created successfully";
-                    } 
-                    else {
-                        echo "Error: " . $sql2 . "<br>" . $conn->error;
-                    } 
+                    echo "<div id='Test'>";
+                    echo "Starting time: " . $row['TimeS'];
+                    echo "<br> Ending time: " . $row['TimeE'];
+                    echo "<br> Posts date: " . $row['Date'];
+                    echo "<br> Comment: " . $row['Comment'];
+                    echo "</div>";
                 }
-            } 
-            else{
+                echo "</table>";
+                // Free result set
+                mysqli_free_result($result);
+            } else{
                 echo "No match for Query";
             }
-        }
-    }
-
-
-    $sql = "SELECT * FROM worklog";
-    if($result = mysqli_query($conn, $sql)){
-        if(mysqli_num_rows($result) > 0){
-            while($row = mysqli_fetch_array($result)){
-                echo "<br> Starting time: " . $row['TimeS'];
-                echo "<br> Ending time: " . $row['TimeE'];
-                echo "<br> Posts date: " . $row['Date'];
-                echo "<br> Comment: " . $row['Comment'] . "<br>";
-            }
-            echo "</table>";
-            // Free result set
-            mysqli_free_result($result);
         } else{
-            echo "No match for Query";
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
         }
-    } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-    }
 
 
+    ?>
+    
+    <form method="post">
+    Date: <input type="text" name="Date" required="require" onfocus='(this.type="date")' onblur='(this.type="text")' <?php echo'value="'.$Today.'"' ?>><br>
+    Starting time: <input type="text" name="TimeS" required="require" onfocus='(this.type="time")' onblur='(this.type="text")' <?php echo'value="'.$CurrentTime.'"' ?>><br>
+    Ending time: <input type="text" name="TimeE" required="require" onfocus='(this.type="time")' onblur='(this.type="text")'><br>
+    Kommentar: <input type="text" name="Kommentar" required="require">
+    <input type="submit">
+    </form>
+</div>
 
-echo '<form method="post">';
-echo 'Date: <input type="date" name="Date" required="require" placeholder="'.$Today.'"><br>';
-echo 'Starting time: <input type="time" name="TimeS" required="require"><br>';
-echo 'Ending time: <input type="time" name="TimeE" required="require"><br>';
-echo 'Kommentar: <input type="text" name="Kommentar" required="require" placeholder="'.$Today.'">';
-echo '<input type="submit">';
-echo '</form>';
 
